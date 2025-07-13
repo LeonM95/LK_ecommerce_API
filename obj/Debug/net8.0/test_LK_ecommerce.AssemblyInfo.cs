06083079fab0b1c28a00caacb39892b07d9e863e
LeonM95 <leomasis95@gmail.com>
2025-07-12 18:18:30 -0600

    Adedd CRUD for Adderss, Category, Product and corrected some issues with existing cotrollers

--- obj/Debug/net8.0/test_LK_ecommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/test_LK_ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test_LK_ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+403729746740a786f27e743ee28d51483daa2114")]
 [assembly: System.Reflection.AssemblyProductAttribute("test_LK_ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test_LK_ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
